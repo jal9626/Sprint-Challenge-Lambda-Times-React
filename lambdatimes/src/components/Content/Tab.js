5a1219a4705dfc50c3680a4fd4f560b4c2ffa4a0
Ashley <jal9626@gmail.com>
2019-03-25 00:17:35 -0400

    selectedTabHandler not working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -8,7 +8,7 @@ const Tab = props => {
   return (
     <div
       className={'tab'}
-      onClick={props.selectTabHandler(props.tab)}
+      onClick={props.selectTabHandler}
       //   /* Replace this dummy click handler function with your selectTabHandler function from props 
       //    you'll need to pass the `tab` in as an argument to this handler. */
       // }}
